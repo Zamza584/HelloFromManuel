e01202db2fc9a1510fc312de23648c996f6c088e
Zamza584 <mzamalloa97@gmail.com>
2021-05-15 13:54:55 -0500

    commited

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,5 @@ public class Main {
         System.out.println("Hello, my name is Manuel");
         System.out.println("Hello Manuel, my name is Domenico.");
         System.out.println("Hello Manuel, this is Ellen");
-        System.out.println("Manuel, what is 2 + 2?");
-        System.out.println("hi there");
-
     }
 }
