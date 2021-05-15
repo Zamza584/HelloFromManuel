eed8398cb8d57875a96c9999da71e2d3be3fe1bb
yllenfer <fer19020@byui.edu>
2021-05-15 13:45:47 -0500

    Manuel, are you smart enough to respond to this?

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello, my name is Manuel");
         System.out.println("Hello Manuel, my name is Domenico.");
         System.out.println("Hello Manuel, this is Ellen");
+        System.out.println("Manuel, what is 2 + 2?");
 
     }
 }
